21aed3857af247f2a19cb2cdaa14d62a7ab25047
igor.ribeiro <igorapalvesribeiro@gmail.com>
2021-04-22 21:18:16 -0300

    terminando terceira aula

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import {
     StyleSheet,
@@ -11,6 +12,11 @@ import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
 export function Confirmation() {
+    const navigation = useNavigation();
+    function handleMoveOn(){
+        navigation.navigate('PlantSelect');
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
@@ -28,6 +34,7 @@ export function Confirmation() {
             </Text>
                 <View style={styles.footer}>
                     <Button 
+                        onPress={handleMoveOn}
                         title="Começar"
                     />
                 </View>
