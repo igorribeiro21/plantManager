3d5e925dc76f7f035ff7ed2d7b707d41ce0164fe
igor.ribeiro <igorapalvesribeiro@gmail.com>
2021-04-25 11:33:57 -0300

    terminando quinto dia

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -23,7 +23,7 @@ import { format, isBefore } from 'date-fns';
 import { PlantProps, savePlant } from '../libs/storage';
 
 interface Params {
-    plant: PlantProps       
+    plant: PlantProps
 }
 
 export function PlantSave() {
@@ -51,86 +51,91 @@ export function PlantSave() {
         setShowDatePicker(oldState => !oldState);
     }
 
-    async function handleSave(){
+    async function handleSave() {
         try {
             await savePlant({
                 ...plant,
                 dateTimeNotification: selectedDateTime
             });
 
-            navigation.navigate('Confirmation',{
+            navigation.navigate('Confirmation', {
                 title: 'Tudo certo',
                 subtitle: 'Fique tranquilo que sempre vamos lembrar você de cuidar da sua plantinha com bastante amor.',
                 buttonTitle: 'Muito obrigado :D',
                 icon: 'hug',
                 nextScreen: 'MyPlants'
             });
-        }catch {
+        } catch {
             Alert.alert('Não foi possivel salvar. 😢')
         }
     }
     return (
-        <View style={styles.container}>
-            <View style={styles.plantInfo}>
-                <SvgFromUri
-                    uri={plant.photo}
-                    height={150}
-                    width={150}
-                />
-                <Text style={styles.plantName}>
-                    {plant.name}
-                </Text>
-                <Text style={styles.plantAbout}>
-                    {plant.about}
-                </Text>
-
-            </View>
-            <View style={styles.controller}>
-                <View style={styles.tipContainer}>
-                    <Image
-                        source={waterDrop}
-                        style={styles.tipImage}
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={styles.container}
+        >
+            <View style={styles.container}>
+                <View style={styles.plantInfo}>
+                    <SvgFromUri
+                        uri={plant.photo}
+                        height={150}
+                        width={150}
                     />
-                    <Text style={styles.tipText}>
-                        {plant.water_tips}
+                    <Text style={styles.plantName}>
+                        {plant.name}
+                    </Text>
+                    <Text style={styles.plantAbout}>
+                        {plant.about}
                     </Text>
 
                 </View>
-                <Text style={styles.alertLabel}>
-                    Escolha o melhor horário para ser lembrado:
+                <View style={styles.controller}>
+                    <View style={styles.tipContainer}>
+                        <Image
+                            source={waterDrop}
+                            style={styles.tipImage}
+                        />
+                        <Text style={styles.tipText}>
+                            {plant.water_tips}
+                        </Text>
+
+                    </View>
+                    <Text style={styles.alertLabel}>
+                        Escolha o melhor horário para ser lembrado:
                 </Text>
 
-                {
-                    showDatePicker &&
-                    (
-                        <DateTimePicker
-                            value={selectedDateTime}
-                            mode="time"
-                            display="spinner"
-                            onChange={handleChangeTime}
-                        />
-                    )
-                }
+                    {
+                        showDatePicker &&
+                        (
+                            <DateTimePicker
+                                value={selectedDateTime}
+                                mode="time"
+                                display="spinner"
+                                onChange={handleChangeTime}
+                            />
+                        )
+                    }
 
-                {
-                    Platform.OS === 'android' && (
-                        <TouchableOpacity
-                            style={styles.dateTimePickerButton}
-                            onPress={handleOpenDateTimePickerForAndroid}
-                        >
-                            <Text style={styles.dateTimePickerText}>
-                                {`Mudar ${format(selectedDateTime, 'HH:mm')} `}
-                            </Text>
-                        </TouchableOpacity>
-                    )
-                }
+                    {
+                        Platform.OS === 'android' && (
+                            <TouchableOpacity
+                                style={styles.dateTimePickerButton}
+                                onPress={handleOpenDateTimePickerForAndroid}
+                            >
+                                <Text style={styles.dateTimePickerText}>
+                                    {`Mudar ${format(selectedDateTime, 'HH:mm')} `}
+                                </Text>
+                            </TouchableOpacity>
+                        )
+                    }
 
-                <Button
-                    title="Cadastrar planta"
-                    onPress={handleSave}
-                />
+                    <Button
+                        title="Cadastrar planta"
+                        onPress={handleSave}
+                    />
+                </View>
             </View>
-        </View>
+        </ScrollView>
     );
 }
 
